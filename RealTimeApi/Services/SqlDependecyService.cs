a1e058b7cc1b988b1cc6d5ec1090b74e944e97f0
cesar-flores-bsd <cesar.augusto@bsdenterprise.com>
2019-11-29 18:20:47 -0600

    Add project files.

---
+++ RealTimeApi/Services/SqlDependecyService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using RealTimeApi.Hubs;
+using System.Data.SqlClient;
+
+namespace RealTimeApi.Services
+{
+    public interface IDataChangeNotificationService
+    {
+        void Config();
+    }
+    public class SqlDependecyService : IDataChangeNotificationService
+    {
+        private readonly IConfiguration configuration;
+        private readonly IHubContext<HubNotificacion> hub;
+
+        public SqlDependecyService(IConfiguration configuration, IHubContext<HubNotificacion> hub)
+        {
+            this.configuration = configuration;
+            this.hub = hub;
+        }
+
+        public void Config()
+        {
+            Suscribir();
+        }
+
+        private void Suscribir()
+        {
+            string cns = configuration.GetConnectionString("DefaultConnection");
+            using (var conn = new SqlConnection(cns))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand(@"SELECT [EmployeeID], [FirstName], [LastName], [PhoneNumber] FROM [dbo].[EmployeeList]", conn))
+                {
+                    cmd.Notification = null;
+                    SqlDependency dependency = new SqlDependency(cmd);
+                    dependency.OnChange += Dependency_OnChange;
+                    SqlDependency.Start(cns);
+                    cmd.ExecuteReader();
+                }
+            }
+        }
+
+        private void Dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                string msg = ObtenerMensaje(e);
+                hub.Clients.All.SendAsync("ReceiveMessage", "Admin", msg);
+            }
+            Suscribir();
+        }
+
+        private string ObtenerMensaje(SqlNotificationEventArgs e)
+        {
+            string Mensaje = "";
+            switch (e.Info)
+            {
+                case SqlNotificationInfo.Insert:
+                    Mensaje = "Se ha insertado un registro";
+                    break;
+                case SqlNotificationInfo.Update:
+                    Mensaje = "Se ha actualizado un registro";
+                    break;
+                case SqlNotificationInfo.Delete:
+                    Mensaje = "Se ha Eliminado un registro";
+                    break;
+                default:
+                    Mensaje = "Un cambio desconosido a ocurrido";
+                    break;
+            }
+
+            return Mensaje;
+        }
+    }
+}
---
+++ RealTimeApi/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RealTimeApi.Hubs;
+using RealTimeApi.Services;
+
+namespace RealTimeApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            //{
+            //    builder
+            //    .AllowAnyMethod()
+            //    .AllowAnyHeader()
+            //    .AllowCredentials()
+            //    .WithOrigins("http://localhost:4200", "http://ap.bsdenterprise.com:8095/ChatEjemplo");
+            //}));
+
+            services.AddScoped<IDataChangeNotificationService, SqlDependecyService>();
+            services.AddSignalR();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime, IDataChangeNotificationService notificationService)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //app.UseCors("CorsPolicy");
+            app.UseSignalR(s =>
+            {
+                var desireProtocols = HttpTransportType.WebSockets | HttpTransportType.LongPolling;
+
+                s.MapHub<HubNotificacion>("/NotificacionHub", (options) =>
+                {
+                    options.Transports = desireProtocols;
+                    options.LongPolling.PollTimeout = TimeSpan.FromSeconds(3);
+
+                });
+            });
+
+            app.UseMvc();
+            notificationService.Config();
+        }
+    }
+}
